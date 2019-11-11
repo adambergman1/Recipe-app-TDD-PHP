bf15e7cfeb1c5de3243fa3b130b540ee17c96573
Niklas Nilsson <nn222ia@student.lnu.se>
2019-11-11 17:34:49 +0100

    merge to master

--- TDD/tests/model/RecipeTest.php
+++ TDD/tests/model/RecipeTest.php
@@ -58,7 +58,7 @@ class RecipeTest extends TestCase
     /** @test */
     public function test()
     {
-        // var_dump($this->instruction);
+        var_dump($this->instruction);
         foreach ($this->instructionsCollection->getInstructions() as $key) {
             echo $key->getInstruction();
             $this->assertFalse($key->isCompleted());
