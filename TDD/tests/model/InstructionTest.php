394226b49d6e02333de0cd3c75756d8b17cd72e6
adambrgmn <adam@wordpressutvecklare.se>
2019-11-11 11:48:56 +0100

    RED

---
+++ TDD/tests/model/InstructionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class InstructionTest extends TestCase
+{
+    /** @test */
+    function shouldAcceptInstruction()
+    {
+        $input = 'Turn on the oven at 200 degrees';
+        $sut = new Instruction($input);
+        $actual = $sut->getInstruction();
+
+        $expected = 'Turn on the oven at 200 degrees';
+
+        $this->assertEquals($actual, $expected);
+    }
+}
