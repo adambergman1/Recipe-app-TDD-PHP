28c19fa747bca5ebf510412dc83c74b09d90f9dc
Niklas Nilsson <nn222ia@student.lnu.se>
2019-11-11 15:24:13 +0100

    RED

---
+++ TDD/tests/model/InstructionsCollectionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class InstructionCollectionTest extends TestCase
+{
+    protected $instruction;
+    protected $sut;
+
+    public function setUp(): void
+    {
+        $this->instruction = new Instruction('Set oven to 200°');
+        $this->sut = new InstructionsCollection();
+    }
+
+    /** @test */
+    public function shouldAddInstructionToCollection()
+    {
+        $this->sut->addInstruction($this->instruction);
+
+        $actual = $this->sut->getInstructions();
+
+        $expected = array();
+        $expected[] = 'Set oven to 200°';
+        $this->assertEquals($actual, $expected);
+    }
+}
--- TDD/tests/model/RecipeTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use PHPUnit\Framework\TestCase;
-
-class RecipeTest extends TestCase
-{ }
