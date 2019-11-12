eb2ce532cede3cfec740137354925ba2f01fcfa8
adambrgmn <adam@wordpressutvecklare.se>
2019-11-12 09:30:20 +0100

    GREEN

---
+++ TDD/src/model/Recipe.php
@@ -0,0 +1,16 @@
+<?php
+
+class Recipe
+{
+    private $title;
+
+    public function __construct(string $title)
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+}
