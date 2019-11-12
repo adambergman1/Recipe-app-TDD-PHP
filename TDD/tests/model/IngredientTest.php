5170e51a7b2eb6b5e967009e8aacf66879e83f27
adambrgmn <adam@wordpressutvecklare.se>
2019-11-12 11:38:10 +0100

    RED

--- TDD/tests/model/IngredientTest.php
+++ TDD/tests/model/IngredientTest.php
@@ -4,6 +4,39 @@ use PHPUnit\Framework\TestCase;
 
 class IngredientTest extends TestCase
 {
+    protected $amount;
+    protected $measurement;
+
+    function setUp(): void
+    {
+        $this->amount = $this->getMockBuilder("Amount")
+            ->setConstructorArgs([2.3])
+            ->setMethods(["getAmount"])
+            ->getMock();
+
+        $this->amount->method("getAmount")
+            ->willReturn(2.3);
+
+        $this->measurement = $this->getMockBuilder("Measurement")
+            ->setConstructorArgs(["dl"])
+            ->setMethods(["getMeasurement"])
+            ->getMock();
+
+        $this->measurement->method("getMeasurement")
+            ->willReturn("dl");
+    }
+
+    /** @test */
+    function shouldAcceptIngredientWithAmountAndMeasurement()
+    {
+        $input = 'Potatoes';
+
+        $sut = new Ingredient($input);
+        $actual = $sut->getName();
+        $expected = 'Potatoes';
+        $this->assertEquals($actual, $expected);
+    }
+
     /** @test */
     function shouldAcceptIngredient()
     {
