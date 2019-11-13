cadcb6c54e4feea078da9d9bd5334b9142c78b95
adambrgmn <adam@wordpressutvecklare.se>
2019-11-13 13:53:52 +0100

    GREEN

--- TDD/src/view/RecipeView.php
+++ TDD/src/view/RecipeView.php
@@ -6,4 +6,29 @@ class RecipeView
     {
         return "<h1>Cook book</h1>";
     }
+
+    public function generateFirstRecipeSection()
+    {
+        return '
+        <input type="text" name="title" placeholder="Title" value=""/>
+        <input type="text" name="author" placeholder="Author" value=""/>
+
+        <label for="servings">Servings:</label>
+        <select id="servings">
+            <option value="two">2</option>
+            <option value="four">4</option>
+            <option value="six">6</option>
+            <option value="eight">8</option>
+            <option value="ten">10</option>
+            <option value="twelve">12</option>
+        </select>
+
+        <label for="tag">Tag</label>
+        <select id="tag">
+            <option value="breakfast">Breakfast</option>
+            <option value="lunch">Lunch</option>
+            <option value="dinner">Dinner</option>
+        </select>
+        ';
+    }
 }
