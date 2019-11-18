d68509cb784d0ce6dc6b066d7467d222a0f3cc46
Niklas Nilsson <nn222ia@student.lnu.se>
2019-11-18 15:11:37 +0100

    RED

--- TDD/src/index.php
+++ TDD/src/index.php
@@ -3,8 +3,13 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 ini_set('display_startup_errors', 'On');
 
+require_once("view/MainView.php");
 require_once("view/AddRecipeView.php");
+
 require_once("model/RecipeFactory.php");
+
+$MainView = new MainView();
+echo $MainView->generateMainTitle();
 
 $view = new AddRecipeView(new RecipeFactory());
 $view->generateOutput();
--- TDD/tests/view/MainViewTest.php
+++ TDD/tests/view/MainViewTest.php
@@ -10,9 +10,21 @@ class MainViewTest extends TestCase
         $view = new MainView();
 
         $actual = $view->generateMainTitle();
-
         $expected = '<h1>My Cook Book</h1>';
 
         $this->assertEquals($actual, $expected);
+    }
+
+    /** @test */
+    public function shouldCallOnGenerateMainTitle()
+    {
+        $viewMock = $this->createMock(MainView::class);
+
+        // $title = '<h1>My Cook Book</h1>';
+        // $viewMock->method('generateMainTitle')->willReturn($title);
+
+        $viewMock->expects($this->once())->method('generateMainTitle');
+
+        $viewMock->render();
     }
 }
