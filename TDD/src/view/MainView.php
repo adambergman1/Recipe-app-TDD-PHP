7ed0c65530c90970030136e553a85e80c44126b8
Niklas Nilsson <nn222ia@student.lnu.se>
2019-11-19 14:08:16 +0100

    GREEN

--- TDD/src/view/MainView.php
+++ TDD/src/view/MainView.php
@@ -3,14 +3,14 @@
 class MainView
 {
 
-  public function generateMainTitle()
-  {
-    return '<h1>My Cook Book</h1>';
-  }
+    public function generateMainTitle()
+    {
+        return '<h1>My Cook Book</h1>';
+    }
 
-  public function render($view, $collection = null)
-  {
-    return '<!DOCTYPE html>
+    public function render($view, $collection = null)
+    {
+        return '<!DOCTYPE html>
         <html lang="en">
           <head>
             <meta charset="utf-8">
@@ -25,40 +25,53 @@ class MainView
            </body>
         </html>
       ';
-  }
-
-  public function renderRecipes($collection)
-  {
-    if ($collection == null || empty($collection->getRecipes())) {
-      return '<p>No recipes</p>';
     }
 
-    $output = "";
-    foreach ($collection->getRecipes() as $recipe) {
-      $output .= "<div class='recipe'>";
-      $output .= '<div class="recipe-title">' . $recipe->getTitle() . '</div>';
-      $output .= '<div class="recipe-author">' . $recipe->getAuthor() . '</div>';
-      $output .= '<div class="recipe-servings">' . $recipe->getServings() . '</div>';
-      $output .= '<div class="recipe-tag">' . $recipe->getTagName() . '</div>';
-      $output .= '<div class="recipe-ingredients">' . $this->renderIngredients($recipe->getIngredients()) . '</div>';
-      // $output .= '<div class="recipe-instructions">' . $this->renderInstructions() . '</div>';
-      $output .= "</div>";
+    public function renderRecipes($collection)
+    {
+        if ($collection == null || empty($collection->getRecipes())) {
+            return '<p>No recipes</p>';
+        }
+
+        $output = "";
+        foreach ($collection->getRecipes() as $recipe) {
+            $output .= "<div class='recipe'>";
+            $output .= '<div class="recipe-title">' . $recipe->getTitle() . '</div>';
+            $output .= '<div class="recipe-author">' . $recipe->getAuthor() . '</div>';
+            $output .= '<div class="recipe-servings">' . $recipe->getServings() . '</div>';
+            $output .= '<div class="recipe-tag">' . $recipe->getTagName() . '</div>';
+            $output .= '<div class="recipe-ingredients">' . $this->renderIngredients($recipe->getIngredients()) . '</div>';
+            $output .= '<div class="recipe-instructions">' . $this->renderInstructions($recipe->getInstructions()) . '</div>';
+            $output .= "</div>";
+        }
+
+        return $output;
     }
 
-    return $output;
-  }
+    public function renderIngredients(array $ingredients)
+    {
+        $output = '';
+        foreach ($ingredients as $ing) {
+            $output .= '<div class="ingredient">';
+            $output .= '<span class="ingredient-amount">' . $ing->getAmount() . '</span>';
+            $output .= '<span class="ingredient-measurement">' . $ing->getMeasurement() . '</span>';
+            $output .= '<span class="ingredient-name">' . $ing->getName() . '</span>';
+            $output .= '</div>';
+        }
 
-  public function renderIngredients(array $ingredients)
-  {
-    $output = '';
-    foreach ($ingredients as $ing) {
-      $output .= '<div class="ingredient">';
-      $output .= '<span class="ingredient-amount">' . $ing->getAmount() . '</span>';
-      $output .= '<span class="ingredient-measurement">' . $ing->getMeasurement() . '</span>';
-      $output .= '<span class="ingredient-name">' . $ing->getName() . '</span>';
-      $output .= '</div>';
+        return $output;
     }
 
-    return $output;
-  }
+    public function renderInstructions(InstructionsCollection $instructions)
+    {
+        $output = '';
+
+        foreach ($instructions->getInstructions() as $instruction) {
+            $output .= '<div class="instruction">';
+            $output .= '<p class="instruction-text">' . $instruction->getInstruction() . '</p>';
+            $output .= '</div>';
+        }
+
+        return $output;
+    }
 }
